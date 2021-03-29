3c9d8b3f8e44ae2d48d49e6ec13a23ac418ae138
David <davidmolinamorales1999@gmail.com>
2021-03-29 02:38:32 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := device/wolder/miTab_PRO_COLORS
 
-ifeq ($(filter wolder), miTab_PRO_COLORS)
+ifeq ($(TARGET_DEVICE), miTab_PRO_COLORS)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
